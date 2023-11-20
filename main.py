d8f2a904d7971aab1ba611e77019e7d0781177d4
CallMeAfiq <151487980+CallMeAfiq@users.noreply.github.com>
2023-11-20 11:56:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+$ pip install python-telegram-bot --upgrade
 from typing import Final 
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
